b0e333f09cec26d27e210e88a6f3b89022ba826f
isaac <ishak.hadj14@gmail.com>
2025-10-17 00:10:23 +0100

    feat: implement the Header logo component

---
+++ src/components/atoms/Logo.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import React from "react";
+
+const Logo = ({ className }: { className?: string }) => {
+  return (
+    <Link href={"/"}>
+      <h2
+        className={cn(
+          "text-2xl text-amber-600 tracking-wider hover:text-shop-orange font-black w-fit group hover-effect",
+          className
+        )}
+      >
+        SHOPCAR
+        <span className="text-shop-orange group-hover:text-amber-600 hover-effect">
+          T
+        </span>
+      </h2>
+    </Link>
+  );
+};
+
+export default Logo;
