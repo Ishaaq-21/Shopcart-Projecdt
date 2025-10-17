486750a36d3a56797cf83ce0f6b61b6cf575911e
isaac <ishak.hadj14@gmail.com>
2025-10-17 21:09:30 +0100

    fix the header import path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Header from "@/components/layout/Header";
+import Header from "@/components/layout/Header/Header";
 
 export const metadata: Metadata = {
   // Title configuration
