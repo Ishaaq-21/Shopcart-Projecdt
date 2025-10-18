bf05235d68223e638b9a75a5f7e19b73bb3c7a9a
isaac <ishak.hadj14@gmail.com>
2025-10-18 22:16:57 +0100

    use the useOutsideClick hook

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -5,19 +5,21 @@ import HeaderNav from "./Header/HeaderNav";
 import { X } from "lucide-react";
 import IconLinks from "./Header/IconLinks";
 import SocialLinks from "../ui/customUI/SocialLinks";
+import { useOutsideClick } from "@/hooks/UseOutsideClick";
 
 type SideBarProps = {
   isOpen: boolean;
   onClose: () => void;
 };
 const SideBar = ({ isOpen, onClose }: SideBarProps) => {
+  const sideRef = useOutsideClick<HTMLDivElement>(onClose);
   return (
     <div
       className={`side-bar fixed ${
         isOpen ? "left-0" : "-left-full"
       } !top-0 z-50 w-full h-[100vh] bg-black/50 hover-effect`}
     >
-      <div className="h-full w-3/4 bg-black p-7">
+      <div ref={sideRef} className="h-full w-3/4 bg-black p-7">
         <div className="side-bar-header flex-between mb-7">
           <Logo className="text-shop-light-bg text-xl sm:text-2xl" />
           <button onClick={onClose}>
