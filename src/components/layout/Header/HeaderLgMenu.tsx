ca1ed97d3b6dd628e7e0787015541113553da462
isaac <ishak.hadj14@gmail.com>
2025-10-18 02:10:07 +0100

    refactor: use the IconLinks component instead of a custom ones

--- src/components/layout/Header/HeaderLgMenu.tsx
+++ src/components/layout/Header/HeaderLgMenu.tsx
@@ -1,20 +1,12 @@
-import CartIcon from "@/components/atoms/CartIcon";
-import FavIcon from "@/components/atoms/FavIcon";
-import SignIn from "@/components/atoms/SignIn";
 import React from "react";
 import HeaderNav from "./HeaderNav";
-import SearchIcon from "@/components/atoms/SearchIcon";
+import IconLinks from "./IconLinks";
 
 const HeaderLgMenu = () => {
   return (
     <>
       <HeaderNav />
-      <div className="gap-5 hidden md:flex justify-between items-center">
-        <SearchIcon />
-        <CartIcon />
-        <FavIcon />
-        <SignIn />
-      </div>
+      <IconLinks />
     </>
   );
 };
