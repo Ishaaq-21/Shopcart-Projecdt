5dc09f3606460f4d09ec1a6ce2789ad5a642bf31
isaac <ishak.hadj14@gmail.com>
2025-10-20 23:41:51 +0100

    refactor: change the layout strcuture to isolate Sanity Studio from shop layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,13 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Header from "@/components/layout/Header/Header";
-import { ClerkProvider } from "@clerk/nextjs";
-import Footer from "@/components/layout/Footer/Footer";
-export const metadata: Metadata = {
-  // Title configuration
-  title: {
-    template: "%s - Shopcart online store",
-    default: "Shopcart online store",
-  },
-
-  // Site description
-  description: "Shopcart online store, Your one stop shop for all your needs",
-};
-
-export default function RootLayout({
+const RootLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) => {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={`font-poppins antialiased`}>
-          <Header />
-          {children}
-          <Footer />
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`font-poppins antialiased`}>{children}</body>
+    </html>
   );
-}
+};
+
+export default RootLayout;
