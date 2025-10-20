64ee32ae6e30d8072ad6a4b960d446793ccf0aa4
isaac <ishak.hadj14@gmail.com>
2025-10-20 03:22:40 +0100

    add types and data for the footer component data

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -21,3 +21,59 @@ export const headerMenuData: HeaderMenuItemType[] = [
     href: "/deal",
   },
 ];
+
+//Quick Links in the footer --------------------------
+export type QuickLinkTitle =
+  | "About us"
+  | "Contact us"
+  | "Terms & Conditions"
+  | "Privacy Policy"
+  | "FAQs"
+  | "Help";
+
+export type QuickLinkHref =
+  | "/about"
+  | "/contact"
+  | "/terms"
+  | "/privacy"
+  | "/faqs"
+  | "/help";
+export type QuickLink = {
+  title: QuickLinkTitle;
+  href: QuickLinkHref;
+};
+export const quickLinksData: QuickLink[] = [
+  { title: "About us", href: "/about" },
+  { title: "Contact us", href: "/contact" },
+  { title: "Terms & Conditions", href: "/terms" },
+  { title: "Privacy Policy", href: "/privacy" },
+  { title: "FAQs", href: "/faqs" },
+  { title: "Help", href: "/help" },
+];
+
+//-----------------------------------------------------
+
+// Categories in the footer ------------------------------
+export type Category = {
+  title: string;
+  href:
+    | "mobiles"
+    | "appliances"
+    | "smartphones"
+    | "air-conditioners"
+    | "washing-machine"
+    | "kitchen-appliances"
+    | "gadget-accessories";
+};
+
+// 2️⃣ Initialize the array with that strict type
+export const categoriesData: Category[] = [
+  { title: "Mobiles", href: "mobiles" },
+  { title: "Appliances", href: "appliances" },
+  { title: "Smartphones", href: "smartphones" },
+  { title: "Air Conditioners", href: "air-conditioners" },
+  { title: "Washing Machine", href: "washing-machine" },
+  { title: "Kitchen Appliances", href: "kitchen-appliances" },
+  { title: "gadget accessories", href: "gadget-accessories" },
+];
+//-------------------------------------------------------------
