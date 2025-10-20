6779180db2d079aa836b707511beb1aa171b9bef
isaac <ishak.hadj14@gmail.com>
2025-10-21 00:41:15 +0100

    add the schema types in the types arr

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,6 +1,24 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { CategoryType } from "./CategoryType";
+import { productType } from "./productType";
+import { orderType } from "./orderType";
+import { blogType } from "./BlogType";
+import { blogCategoryType } from "./blogCategoryType";
+import { blockContentType } from "./blockContentType";
+import { authorType } from "./authorType";
+import { brandType } from "./brandType";
+import { addressType } from "./adressType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [CategoryType],
+  types: [
+    CategoryType,
+    productType,
+    orderType,
+    blogType,
+    blogCategoryType,
+    authorType,
+    brandType,
+    addressType,
+    blockContentType,
+  ],
 };
