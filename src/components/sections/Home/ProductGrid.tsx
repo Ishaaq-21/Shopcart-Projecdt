d44fa5f8cf16d33eb9d5d61bc8e31b4e4a8c66d3
isaac <ishak.hadj14@gmail.com>
2025-10-24 23:50:59 +0100

    feat(products' grid): build the grid container for the products

---
+++ src/components/sections/Home/ProductGrid.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import HomeTab from "./HomeTab";
+import { productType } from "@/constants/data";
+import { client } from "@/sanity/lib/client";
+import ProductCard from "@/components/ui/customUI/ProductCard";
+import Loader from "@/components/ui/customUI/Loader";
+import NoProductAvailable from "@/components/ui/customUI/NoProductsAvailable";
+import { Product } from "../../../../sanity.types";
+
+const ProductGrid = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedTab, setSelectedTab] = useState(productType[0]?.title || "");
+  const query = `*[_type == "product" && variant == $variant] | order(name asc){
+  ...,"categories": categories[]->title
+}`;
+  const params = { variant: selectedTab.toLowerCase() };
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setIsLoading(true);
+        const productsData = await client.fetch(query, params);
+        setProducts(productsData);
+        console.log(productsData);
+      } catch (error) {
+        console.error("Error", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchProducts();
+  }, [selectedTab]);
+
+  return (
+    <div className="py-10">
+      <HomeTab selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+      {isLoading && <Loader />}
+      {!products.length ? (
+        <NoProductAvailable selectedTab={selectedTab} />
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 py-10">
+          {products.map((product: Product) => (
+            <ProductCard key={product?._id} product={product} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProductGrid;
