7d730ad3dff529eb2b89f1cafad378d7e573c158
isaac <ishak.hadj14@gmail.com>
2025-10-25 17:35:56 +0100

    feat(add-to-cart): disable button and update style when product is out of stock

--- src/components/atoms/AddToCartBtn.tsx
+++ src/components/atoms/AddToCartBtn.tsx
@@ -1,11 +1,16 @@
 import { ShoppingBag } from "lucide-react";
 import React from "react";
+import { Product } from "../../../sanity.types";
 
-const AddToCartBtn = () => {
+const AddToCartBtn = ({ product }: { product: Product }) => {
+  const isOutOfStock = product?.stock === 0;
   return (
-    <button className="flex w-fit items-center  bg-shop-orange font-semibold  gap-2 mt-2 py-2 px-2 md:px-4 rounded-lg text-sm tracking-wider text-white hover:bg-shop-orange/70 hover-effect">
+    <button
+      className={`flex w-fit items-center font-semibold  gap-2 mt-2 py-2 px-2 md:px-4 rounded-lg text-sm tracking-wider   hover-effect ${isOutOfStock ? "bg-shop-orange/70 text-black/70" : "bg-shop-orange hover:bg-shop-orange/70 text-white"}`}
+      disabled={isOutOfStock}
+    >
       <ShoppingBag className="w-5 h-5" />
-      Add to cart
+      {isOutOfStock ? "Out of Stock" : "Add to cart"}
     </button>
   );
 };
