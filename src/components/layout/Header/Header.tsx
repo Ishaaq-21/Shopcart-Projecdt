a7e319d67852f6e4ad1f26c3a08157d29903984d
isaac <ishak.hadj14@gmail.com>
2025-10-25 23:39:11 +0100

    style: add bottom margin to prevent page content from overlapping with fixed header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -7,7 +7,7 @@ import MobileMenu from "./MobileMenu";
 
 const Header = () => {
   return (
-    <header className="bg-white py-5 fixed inset-0 z-50 h-[70px]">
+    <header className="bg-white py-5 fixed inset-0 z-50 h-[70px] mb-[70px]">
       <Container className="flex-between">
         <Logo />
         <HeaderLgMenu />
