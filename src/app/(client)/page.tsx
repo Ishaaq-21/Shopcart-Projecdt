84194f9d6558a7972871bd78839cd8ca3db3ff3a
isaac <ishak.hadj14@gmail.com>
2025-10-27 23:44:36 +0100

    build home page

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,13 +1,19 @@
 import Container from "@/components/common/Container";
 import HomeBanner from "@/components/sections/Home/HomeBanner";
+import HomeCategories from "@/components/sections/Home/HomeCategories";
+import LatestBlogs from "@/components/sections/Home/LatestBlogs";
+import ProductGrid from "@/components/sections/Home/ProductGrid";
+import ShopByBrands from "@/components/sections/Home/ShopByBrands";
 
 const Home = () => {
   return (
-    <main className="mb-20 pt-[70px]">
-      <Container>
-        <HomeBanner />
-      </Container>
-    </main>
+    <Container>
+      <HomeBanner />
+      <ProductGrid />
+      <HomeCategories />
+      <ShopByBrands />
+      <LatestBlogs />
+    </Container>
   );
 };
 
