32e31a7729a26c7d29f16c11548ca1aa7f39e2d3
isaac <ishak.hadj14@gmail.com>
2025-10-28 21:23:13 +0100

    feat: wrap the container in a parent div + add a break point for our grid columns

--- src/app/(client)/deal/page.tsx
+++ src/app/(client)/deal/page.tsx
@@ -4,21 +4,28 @@ import { getDealProducts } from "@/sanity/queries";
 import React from "react";
 import { Product } from "../../../../sanity.types";
 import Container from "@/components/common/Container";
+import Title from "@/components/atoms/Title";
 
 const DealPage = async () => {
   const dealProducts = await getDealProducts();
   return (
-    <Container>
-      {dealProducts.length ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 py-10">
-          {dealProducts.map((product: Product, index: number) => (
-            <ProductCard key={product?._id} product={product} index={index} />
-          ))}
-        </div>
-      ) : (
-        <NoProductAvailable />
-      )}
-    </Container>
+    <div className="py-10 bg-deal-bg">
+      <Container>
+        <Title className="mb-5 pb-2 border-b border-black/20 w-fit ">
+          {" "}
+          Hot Deals of the week
+        </Title>
+        {dealProducts.length ? (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-5">
+            {dealProducts.map((product: Product, index: number) => (
+              <ProductCard key={product?._id} product={product} index={index} />
+            ))}
+          </div>
+        ) : (
+          <NoProductAvailable />
+        )}
+      </Container>
+    </div>
   );
 };
 
