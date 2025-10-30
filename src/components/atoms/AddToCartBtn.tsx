c76b1fbc6318f411ab28f93521a002ffda440aa1
isaac <ishak.hadj14@gmail.com>
2025-10-30 01:22:13 +0100

    style: change bg-colors, and add a className prop for custom styling

--- src/components/atoms/AddToCartBtn.tsx
+++ src/components/atoms/AddToCartBtn.tsx
@@ -2,11 +2,17 @@ import { ShoppingBag } from "lucide-react";
 import React from "react";
 import { Product } from "../../../sanity.types";
 
-const AddToCartBtn = ({ product }: { product: Product }) => {
+const AddToCartBtn = ({
+  product,
+  className,
+}: {
+  product: Product;
+  className?: string;
+}) => {
   const isOutOfStock = product?.stock === 0;
   return (
     <button
-      className={`flex w-fit items-center font-semibold  gap-2 mt-2 py-2 px-2 md:px-4 rounded-lg text-sm tracking-wider   hover-effect ${isOutOfStock ? "bg-shop-orange/70 text-black/70" : "bg-shop-orange hover:bg-shop-orange/70 text-white"}`}
+      className={`flex w-fit items-center font-semibold  gap-2 mt-2 py-2 px-2 md:px-4 rounded-lg text-sm tracking-wider   hover-effect ${isOutOfStock ? "bg-shop-primary text-black/70" : "bg-shop-primary hover:bg-shop-orange text-white"} ${className}`}
       disabled={isOutOfStock}
     >
       <ShoppingBag className="w-5 h-5" />
