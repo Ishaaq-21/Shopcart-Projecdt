fdbab7035fe5d9e1d469018de71385b1eceecfde
isaac <ishak.hadj14@gmail.com>
2025-11-07 22:29:01 +0100

    feat(shop): build the initial shop page

---
+++ src/components/sections/shop/shop.tsx
@@ -0,0 +1,50 @@
+"use client";
+import React, { useState } from "react";
+import { Brand, Category, Product } from "../../../../sanity.types";
+import Container from "@/components/common/Container";
+import Title from "@/components/atoms/Title";
+import ShopAside from "./ShopAside";
+import { useSearchParams } from "next/navigation";
+
+export type ShopProps = {
+  categories: Category[];
+  brands: Brand[];
+};
+const Shop = ({ categories, brands }: ShopProps) => {
+  const searchParams = useSearchParams();
+  const brandParams = searchParams.get("brand");
+  const categoryParams = searchParams.get("category");
+  const [products, setproducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const [selectedCategory, setSelectedCategory] = useState(
+    categoryParams || null
+  );
+  const [selectedBrand, setSelectedBrand] = useState(brandParams || null);
+  const [selectedPrice, setSelectedPrice] = useState("");
+  return (
+    <div>
+      {" "}
+      <Container>
+        <div className="sticky flex justify-between items-center border-b border-shop-primary w-full pb-5">
+          <Title className="font-bold uppercase !text-lg">
+            Get the products based on your needs
+          </Title>
+          <button className="text-lg text-shop-orange hover:text-orange-500 hover-effect underline">
+            Reset filters
+          </button>
+        </div>
+        <ShopAside
+          categories={categories}
+          brands={brands}
+          category={selectedCategory}
+          setCategory={setSelectedCategory}
+          brand={selectedBrand}
+          setBrand={setSelectedBrand}
+        />
+      </Container>
+    </div>
+  );
+};
+
+export default Shop;
