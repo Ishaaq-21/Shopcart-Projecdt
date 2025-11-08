f8a5b3b6df501f4b65431c62d1ecb5a75649695e
isaac <ishak.hadj14@gmail.com>
2025-11-08 01:24:03 +0100

    feat(dynamic): complete building the client shop component that has filter functionality

--- src/components/sections/shop/shop.tsx
+++ src/components/sections/shop/shop.tsx
@@ -1,50 +1,107 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Brand, Category, Product } from "../../../../sanity.types";
 import Container from "@/components/common/Container";
 import Title from "@/components/atoms/Title";
 import ShopAside from "./ShopAside";
-import { useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { client } from "@/sanity/lib/client";
+import Loader from "@/components/ui/customUI/Loader";
+import NoProductAvailable from "@/components/ui/customUI/NoProductsAvailable";
+import ProductCard from "@/components/ui/customUI/ProductCard";
 
 export type ShopProps = {
   categories: Category[];
   brands: Brand[];
 };
 const Shop = ({ categories, brands }: ShopProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
   const searchParams = useSearchParams();
   const brandParams = searchParams.get("brand");
   const categoryParams = searchParams.get("category");
-  const [products, setproducts] = useState<Product[]>([]);
-  const [loading, setLoading] = useState(false);
 
+  // States
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
   const [selectedCategory, setSelectedCategory] = useState(
     categoryParams || null
   );
   const [selectedBrand, setSelectedBrand] = useState(brandParams || null);
   const [selectedPrice, setSelectedPrice] = useState<string | null>(null);
+
+  const fetchProducts = async () => {
+    try {
+      setProducts([]);
+      setError(null);
+      setLoading(true);
+
+      const params = new URLSearchParams();
+
+      if (selectedCategory) params.set("category", selectedCategory);
+      if (selectedBrand) params.set("brand", selectedBrand);
+      if (selectedPrice) params.set("price", selectedPrice);
+
+      router.replace(`${pathname}?${params.toString()}`);
+
+      let minPrice = 0;
+      let maxPrice = 1000;
+      if (selectedPrice) {
+        const [min, max] = selectedPrice.split("-").map(Number);
+        minPrice = min;
+        maxPrice = max;
+      }
+      const query = `
+      *[_type == 'product'
+        && (!defined($selectedCategory) || references(*[_type == "category" && slug.current ==$selectedCategory]._id))
+        && (!defined($selectedBrand) || references(*[_type == "brand" && slug.current == $selectedBrand]._id))
+        && price >= $minPrice && price <= $maxPrice
+      ]
+      | order(name asc) {
+        ...,"categories": categories[]->title
+      }
+    `;
+      const data = await client.fetch(
+        query,
+        { selectedCategory, selectedBrand, minPrice, maxPrice },
+        { next: { revalidate: 0 } }
+      );
+      setProducts(data);
+    } catch (error) {
+      console.error("Error -> ", error);
+      setError(
+        "Something went wrong when trying to fetch products\nPlease try again !!"
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, [selectedBrand, selectedCategory, selectedPrice]);
   return (
-    <div>
-      {" "}
-      <Container>
-        <div className="sticky flex justify-between items-center border-b border-shop-primary w-full pb-5">
-          <Title className="font-bold uppercase !text-lg">
-            Get the products based on your needs
-          </Title>
-          {(selectedCategory !== null ||
-            selectedBrand !== null ||
-            selectedPrice !== null) && (
-            <button
-              onClick={() => {
-                setSelectedCategory(null);
-                setSelectedBrand(null);
-                setSelectedPrice(null);
-              }}
-              className="text-lg text-shop-orange hover:text-orange-500 hover-effect underline"
-            >
-              Reset Filters
-            </button>
-          )}
-        </div>
+    <Container>
+      <div className="sticky flex justify-between items-center border-b border-shop-primary w-full pb-5">
+        <Title className="font-bold uppercase !text-lg">
+          Get the products based on your needs
+        </Title>
+        {(selectedCategory !== null ||
+          selectedBrand !== null ||
+          selectedPrice !== null) && (
+          <button
+            onClick={() => {
+              setSelectedCategory(null);
+              setSelectedBrand(null);
+              setSelectedPrice(null);
+            }}
+            className="text-lg text-shop-orange hover:text-orange-500 hover-effect underline"
+          >
+            Reset Filters
+          </button>
+        )}
+      </div>
+      <div className="flex flex-col md:flex-row gap-10 ">
         <ShopAside
           categories={categories}
           brands={brands}
@@ -55,8 +112,24 @@ const Shop = ({ categories, brands }: ShopProps) => {
           selectedPrice={selectedPrice}
           setSelectedPrice={setSelectedPrice}
         />
-      </Container>
-    </div>
+        {error && (
+          <p className="text-red-500 text-lg font-semibold flex-center w-full h-full">
+            {error}
+          </p>
+        )}
+        {loading ? (
+          <Loader />
+        ) : !products.length ? (
+          <NoProductAvailable selectedTab={""} />
+        ) : (
+          <div className="scrollbar-hide overflow-y-scroll grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 pt-5">
+            {products.map((product: Product, index) => (
+              <ProductCard key={product?._id} product={product} index={index} />
+            ))}
+          </div>
+        )}
+      </div>
+    </Container>
   );
 };
 
