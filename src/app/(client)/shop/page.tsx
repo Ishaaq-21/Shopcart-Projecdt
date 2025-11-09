4b198cbcc9d86a0d4b9bd23ec13a4346d3315f55
isaac <ishak.hadj14@gmail.com>
2025-11-10 00:00:47 +0100

    refactor(get-intial-products): use the new getAllproducts function to retrieve the intial products

--- src/app/(client)/shop/page.tsx
+++ src/app/(client)/shop/page.tsx
@@ -1,22 +1,12 @@
 import Shop from "@/components/sections/shop/shop";
-import { getAllBrands, getCategories } from "@/sanity/queries";
+import { getAllBrands, getAllProducts, getCategories } from "@/sanity/queries";
 import React from "react";
-import { Product } from "../../../../sanity.types";
-import { client } from "@/sanity/lib/client";
-const initialProductQuery = `
-  *[_type == 'product'][0...9] | order(name asc) { 
-    ..., "categories": categories[]->title 
-  }
-`;
+
 const page = async () => {
   const [categories, brands, initialProducts] = await Promise.all([
     getCategories(),
     getAllBrands(),
-    client.fetch<Product[]>(
-      initialProductQuery,
-      {},
-      { next: { revalidate: 3600 } }
-    ),
+    getAllProducts(),
   ]);
   return (
     <div className="shop-page pt-6">
