8c3711c29786a57f06d6eb03029afb0200ee460f
isaac <ishak.hadj14@gmail.com>
2025-11-11 21:04:27 +0100

    feat(cart-link): wrap the cart icon in a link that refers to the cart page

--- src/components/atoms/CartIcon.tsx
+++ src/components/atoms/CartIcon.tsx
@@ -1,9 +1,10 @@
 import { ShoppingBag } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const CartIcon = () => {
   return (
-    <div className="relative flex-between gap-3">
+    <Link href={"/cart"} className="relative flex-between gap-3">
       <span className="text-gray-400 md:hidden">Cart</span>
 
       <ShoppingBag
@@ -11,7 +12,7 @@ const CartIcon = () => {
         className="w-4 h-4 md:w-5 md:h-5 text-white md:text-black hover:text-shop-primary hover-effect"
       />
       <span className="header-icon-span ">0</span>
-    </div>
+    </Link>
   );
 };
 
