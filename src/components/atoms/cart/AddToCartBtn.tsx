6c7ea7359f9162a03010f95a9002770b4d709f36
isaac <ishak.hadj14@gmail.com>
2025-11-11 22:26:39 +0100

    refactor(change-folder): put all cart related small component in one folder

--- src/components/atoms/cart/AddToCartBtn.tsx
+++ src/components/atoms/cart/AddToCartBtn.tsx
@@ -1,8 +1,8 @@
-"use client";
 import { ShoppingBag } from "lucide-react";
 import React from "react";
-import { Product } from "../../../sanity.types";
+import { Product } from "../../../../sanity.types";
 import { useCartContext } from "@/contexts/Cart/CartContextProvider";
+import ExistingCartItem from "./ExistingCartItem";
 
 const AddToCartBtn = ({
   product,
@@ -12,22 +12,29 @@ const AddToCartBtn = ({
   className?: string;
 }) => {
   const isOutOfStock = product?.stock === 0;
-  const { dispatch } = useCartContext();
+  const { state, dispatch } = useCartContext();
   const handleClick = () => {
     dispatch({
       type: "ADD_ITEM",
       payload: product,
     });
   };
-  return (
+  const productCount = state.getItemCountById(state, product._id);
+  return productCount === 0 ? (
     <button
       onClick={handleClick}
-      className={`flex w-fit items-center font-semibold  gap-2 mt-2 py-2 px-2 md:px-4 rounded-lg text-sm tracking-wider   hover-effect ${isOutOfStock ? "bg-shop-primary text-black/70" : "bg-shop-primary hover:bg-shop-orange text-white"} ${className}`}
+      className={`flex w-fit items-center font-semibold gap-2 mt-2 py-2 px-2 md:px-4 rounded-lg text-sm tracking-wider hover-effect ${
+        isOutOfStock
+          ? "bg-shop-primary text-black/70"
+          : "bg-shop-primary hover:bg-shop-orange text-white"
+      } ${className}`}
       disabled={isOutOfStock}
     >
       <ShoppingBag className="w-5 h-5" />
       {isOutOfStock ? "Out of Stock" : "Add to cart"}
     </button>
+  ) : (
+    <ExistingCartItem product={product} productCount={productCount} />
   );
 };
 
