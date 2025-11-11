3c7bf3c883d588946d2c2f3e5dbdfcebabb07cc8
isaac <ishak.hadj14@gmail.com>
2025-11-11 23:52:12 +0100

    feat(toaster): append the toaster in the body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-
+import { Toaster } from "react-hot-toast";
 const RootLayout = ({
   children,
 }: Readonly<{
@@ -7,7 +7,20 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang="en">
-      <body className={`font-poppins antialiased h-screen`}>{children}</body>
+      <body className={`font-poppins antialiased h-screen`}>
+        {children}
+
+        <Toaster
+          position="bottom-right"
+          toastOptions={{
+            style: {
+              padding: "10px 20px",
+              background: "black",
+              color: "white",
+            },
+          }}
+        />
+      </body>
     </html>
   );
 };
