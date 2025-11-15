d63a3e308ff2719facd75a2ca908e755dc09c292
isaac <ishak.hadj14@gmail.com>
2025-11-15 20:53:22 +0100

    feat(wishlist-toggle): add reducer that toggles a product in the wishlist by removing it if present or adding it if absent

--- src/contexts/favorite/FavoriteReducer.ts
+++ src/contexts/favorite/FavoriteReducer.ts
@@ -3,31 +3,38 @@ import { Product } from "../../../sanity.types";
 export type FavoriteState = {
   favorites: Product[];
 };
-export type FavoriteAction =
-  | { type: "ADD_FAVORITE"; payload: Product }
-  | { type: "REMOVE_FAVORITE"; payload: Product };
+export type FavoriteAction = { type: "FAV_CLICK"; payload: Product };
 
 export const initialState: FavoriteState = {
   favorites: [],
 };
+
+function favoriteClick(state: FavoriteState, product: Product) {
+  const productExists = state.favorites.find(
+    (item) => item._id === product._id
+  );
+
+  //if product exists, we will remove it from the list
+  if (productExists)
+    return {
+      ...state,
+      favorites: state.favorites.filter((item) => item._id !== product._id),
+    };
+
+  return {
+    ...state,
+    favorites: [...state.favorites, product],
+  };
+}
 
 export const favoriteReducer = (
   state: FavoriteState = initialState,
   action: FavoriteAction
 ): FavoriteState => {
   switch (action.type) {
-    case "ADD_FAVORITE":
-      return {
-        ...state,
-        favorites: [...state.favorites, action.payload],
-      };
-    case "REMOVE_FAVORITE":
-      return {
-        ...state,
-        favorites: state.favorites.filter(
-          (item) => item._id !== action.payload._id
-        ),
-      };
+    case "FAV_CLICK":
+      return favoriteClick(state, action.payload);
+
     default:
       return state;
   }
