3f6c9f4c0e7980947724d8f0e2780fe18f58d85f
isaac <ishak.hadj14@gmail.com>
2025-11-15 09:22:54 +0100

    feat: fix the layout issue + finish building the component

--- src/components/sections/cart/ProductCart.tsx
+++ src/components/sections/cart/ProductCart.tsx
@@ -4,12 +4,12 @@ import { urlFor } from "@/sanity/lib/image";
 import { CartItem } from "@/types/types";
 import FavIcon from "@/components/atoms/FavIcon";
 import { Trash } from "lucide-react";
-import ExistingCartItem from "@/components/atoms/cart/ExistingCartItem";
+import IncrementDecrementQuantity from "@/components/atoms/cart/IncrementDecrementQuantity";
 
 // I need to fix the layout of this.
 const ProductCart = ({ cartItem }: { cartItem: CartItem }) => {
   return (
-    <div className="flex flex-col md:flex-row gap-5 w-full">
+    <div className="flex flex-col md:flex-row gap-5 bg-white p-5 border-b">
       {cartItem.product.images && (
         <div className="image relative size-44 border p-5">
           <Image
@@ -19,32 +19,36 @@ const ProductCart = ({ cartItem }: { cartItem: CartItem }) => {
           />
         </div>
       )}
-      <div className="flex justify-between items-start self-start">
-        <div>
-          <h3 className="font-semibold text-lg ">{cartItem.product.name}</h3>
-          <span className="text-gray-600 block my-3">
-            variant :{" "}
-            <span className="font-semibold text-black">
-              {cartItem.product.variant}
-            </span>
-          </span>
-          <span className="text-gray-600 block">
-            Statue :{" "}
-            <span className="font-semibold text-black">
-              {cartItem.product.status}
-            </span>
-          </span>
-        </div>
-        <p className="price font-semibold text-lg">
-          $
-          {cartItem.product.price && cartItem.product.price * cartItem.quantity}
-        </p>
-        <div className="flex justify-between items-center">
+      <div className="flex flex-col gap-y-10 justify-between items-start self-start flex-1">
+        <div className="flex justify-between w-full">
           <div>
-            <FavIcon />
-            <Trash size={30} />
+            <h3 className="font-semibold text-lg ">{cartItem.product.name}</h3>
+            <span className="text-gray-600 block my-3">
+              variant :{" "}
+              <span className="font-semibold text-black">
+                {cartItem.product.variant}
+              </span>
+            </span>
+            <span className="text-gray-600 block">
+              Statue :{" "}
+              <span className="font-semibold text-black">
+                {cartItem.product.status}
+              </span>
+            </span>
           </div>
-          <ExistingCartItem product={cartItem.product} />
+          <p className="price font-semibold text-lg">
+            $
+            {cartItem.product.price &&
+              cartItem.product.price * cartItem.quantity}
+          </p>
+        </div>
+
+        <div className="flex justify-between items-center w-full">
+          <div className="flex gap-x-5">
+            <FavIcon />
+            <Trash size={20} />
+          </div>
+          <IncrementDecrementQuantity product={cartItem.product} />
         </div>
       </div>
     </div>
