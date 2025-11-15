daa454a924466654ccd564ef26c4fcd11e37bc4e
isaac <ishak.hadj14@gmail.com>
2025-11-15 09:23:30 +0100

    feat(product-carts): map over the cart-items and display the products informations

--- src/components/sections/cart/CartProducts.tsx
+++ src/components/sections/cart/CartProducts.tsx
@@ -2,21 +2,17 @@
 import React from "react";
 import { useCartContext } from "@/contexts/Cart/CartContextProvider";
 import { CartItem } from "@/types/types";
+import ProductCart from "./ProductCart";
 
 const CartProducts = () => {
   const { state } = useCartContext();
   const cartItems = state.items;
   return (
-    <div>
+    <div className="carts-container">
       {cartItems &&
         cartItems.length > 0 &&
         cartItems.map((item: CartItem) => (
-          <div key={item.product._id}>
-            <span className="block font-semibold text-primary">
-              Product Name: {item.product.name}
-            </span>
-            <span>Product Stock : {item.product.stock}</span>
-          </div>
+          <ProductCart key={item.product._id} cartItem={item} />
         ))}
     </div>
   );
