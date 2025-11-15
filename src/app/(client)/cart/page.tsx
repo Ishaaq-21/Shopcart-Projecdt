d7b9081e83a5bfc85a09989e1db6266107a5c17b
isaac <ishak.hadj14@gmail.com>
2025-11-15 09:25:32 +0100

    style: add shopping bag icon to the title | adjust the gray bg slightly

--- src/app/(client)/cart/page.tsx
+++ src/app/(client)/cart/page.tsx
@@ -1,13 +1,17 @@
 import Title from "@/components/atoms/Title";
 import Container from "@/components/common/Container";
 import CartProducts from "@/components/sections/cart/CartProducts";
+import { ShoppingBag } from "lucide-react";
 import React from "react";
 
 const page = () => {
   return (
-    <div className="pt-10">
+    <div className="py-10 bg-gray-100">
       <Container>
-        <Title className="mb-10">Shopping Cart </Title>
+        <Title className="mb-10 flex gap-2 items-center tracking-wide">
+          <ShoppingBag />
+          Shopping Cart{" "}
+        </Title>
         <CartProducts />
       </Container>
     </div>
