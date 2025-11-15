9d3e1cdc04ccc3f9e036da51539e0ae1dccea5c5
isaac <ishak.hadj14@gmail.com>
2025-11-15 09:20:57 +0100

    refactor: use the new seperate component (incremnt-decrement component)

--- src/components/atoms/cart/ExistingCartItem.tsx
+++ src/components/atoms/cart/ExistingCartItem.tsx
@@ -1,46 +1,16 @@
 import { useCartContext } from "@/contexts/Cart/CartContextProvider";
 import { Product } from "../../../../sanity.types";
-import toast from "react-hot-toast";
+import IncrementDecrementQuantity from "./IncrementDecrementQuantity";
 
-const ExistingCartItem = ({
-  product,
-  productCount,
-}: {
-  product: Product;
-  productCount: number;
-}) => {
-  const { state, dispatch } = useCartContext();
-  const handleIncrement = () => {
-    dispatch({
-      type: "ADD_ITEM",
-      payload: product,
-    });
+const ExistingCartItem = ({ product }: { product: Product }) => {
+  const { state } = useCartContext();
+  const productCount = state.getItemCountById(state, product._id);
 
-    toast.success(`${product.name?.substring(0, 10)}... added successfully`);
-  };
-  const handleDecrement = () => {
-    dispatch({
-      type: "REMOVE_ITEM",
-      payload: product._id,
-    });
-    toast.success(`${product.name?.substring(0, 10)}... removed successfully`);
-  };
   return (
     <div>
       <div className="quantity flex justify-between items-center">
         <span className="text-sm text-black/70">Quantity</span>
-        <div className="flex gap-3 items-center">
-          {" "}
-          <button onClick={handleDecrement} className="text-xl">
-            {" "}
-            -{" "}
-          </button>
-          {productCount}
-          <button onClick={handleIncrement} className="text-xl">
-            {" "}
-            +{" "}
-          </button>
-        </div>
+        <IncrementDecrementQuantity product={product} />
       </div>
       <hr className="my-2" />
       <div className="subtotal flex justify-between items-center font-semibold text-sm">
