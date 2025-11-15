09d1e1075ef97ca6139fbb9198674252725c32a5
isaac <ishak.hadj14@gmail.com>
2025-11-15 20:53:54 +0100

    feat(wish-list): build the initial wishlist page

---
+++ src/app/(client)/wishlist/page.tsx
@@ -0,0 +1,15 @@
+import Container from "@/components/common/Container";
+import WishlistProducts from "@/components/sections/wishlist/wishlistProducts";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <Container>
+        <WishlistProducts />
+      </Container>
+    </div>
+  );
+};
+
+export default page;
