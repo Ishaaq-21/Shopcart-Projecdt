e41ae800f16bf9d24c9b7c65942e3fb010e1edb9
Ishak <ishak.hadj14@gmail.com>
2025-11-16 23:27:50 +0100

    feat(toast): define the reset cart function seperately and add toast on success

--- src/components/atoms/cart/ResetCart.tsx
+++ src/components/atoms/cart/ResetCart.tsx
@@ -1,13 +1,18 @@
 "use client";
 import { useCartContext } from "@/contexts/CartFavContextProvider";
 import React from "react";
+import toast from "react-hot-toast";
 
 const ResetCart = () => {
   const { cartDispatch } = useCartContext();
+  const resetCart = () => {
+    cartDispatch({ type: "RESET_CART" });
+    toast.success("Cart reset successfully");
+  };
   return (
     <button
       className="bg-red-500 text-white px-4 py-2 rounded-md"
-      onClick={() => cartDispatch({ type: "RESET_CART" })}
+      onClick={resetCart}
     >
       Reset Cart
     </button>
