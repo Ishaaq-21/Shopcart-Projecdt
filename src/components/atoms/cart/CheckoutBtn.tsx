d57218e97f7a10dc7e5a71d7945a2aa57ec81458
isaac <ishak.hadj14@gmail.com>
2025-11-22 09:32:51 +0100

    style: add hover effects

--- src/components/atoms/cart/CheckoutBtn.tsx
+++ src/components/atoms/cart/CheckoutBtn.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CheckoutBtn = () => {
   return (
-    <button className="bg-shop-orange w-full rounded-full py-2 text-lg text-white font-semibold">
+    <button className="bg-shop-primary hover:bg-shop-orange hover-effect w-full rounded-full py-2 text-lg text-white font-semibold">
       Proceed to Checkout
     </button>
   );
