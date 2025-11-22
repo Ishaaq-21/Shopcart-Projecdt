42eca4e7adfc1ab007f24b73e3077b88061bc3b8
isaac <ishak.hadj14@gmail.com>
2025-11-22 08:41:01 +0100

    feat(checkout-component): build the intial check-out component

---
+++ src/components/atoms/cart/CheckoutBtn.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CheckoutBtn = () => {
+  return (
+    <button className="bg-shop-orange w-full rounded-full py-2 text-lg text-white font-semibold">
+      Proceed to Checkout
+    </button>
+  );
+};
+
+export default CheckoutBtn;
