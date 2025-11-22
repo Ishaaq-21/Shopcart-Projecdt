710d57b26ab986b18ec589a0fac468ff39eb0b4a
isaac <ishak.hadj14@gmail.com>
2025-11-22 08:41:56 +0100

    feat(custom-class): add a custom className in the props

--- src/components/atoms/SignIn.tsx
+++ src/components/atoms/SignIn.tsx
@@ -1,10 +1,12 @@
 import { SignInButton } from "@clerk/nextjs";
 import React from "react";
 
-const SignIn = () => {
+const SignIn = ({ className }: { className?: string }) => {
   return (
     <SignInButton mode="modal">
-      <button className="self-start text-white/70 md:text-black hover:text-shop-orange hover-effect tracking-wider">
+      <button
+        className={`self-start text-white/70 md:text-black hover:text-shop-orange hover-effect tracking-wider ${className}`}
+      >
         Signin
       </button>
     </SignInButton>
