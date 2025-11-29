2dda7c40912f737636ab6364a7d1ac3db8743923
isaac <ishak.hadj14@gmail.com>
2025-11-29 12:34:18 +0100

    feat(orders-page): build the orders-page

--- src/app/(client)/orders/page.tsx
+++ src/app/(client)/orders/page.tsx
@@ -1,4 +1,12 @@
+import Container from "@/components/common/Container";
+import NoOrders from "@/components/sections/orders/NoOrders";
+import OrdersComponent from "@/components/sections/orders/OrderComponent";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ScrollBar } from "@/components/ui/scroll-area";
+import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { getMyOrders } from "@/sanity/queries";
 import { auth } from "@clerk/nextjs/server";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
 import { redirect } from "next/navigation";
 import React from "react";
 
@@ -7,9 +15,41 @@ const OrdersPage = async () => {
   if (!userId) {
     return redirect("/");
   }
-  const order = await getMyOrders();
+  const orders = await getMyOrders(userId);
 
-  return <div>OrdersPage</div>;
+  return (
+    <Container className="py-10 md:py-16">
+      <Card className="w-full min-h-96">
+        <CardHeader>
+          <CardTitle>Order List</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ScrollArea>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[100px] md:w-auto">
+                    Order Number
+                  </TableHead>
+                  <TableHead className="hidden md:table-cell">Date</TableHead>
+                  <TableHead>Customer</TableHead>
+                  <TableHead className="hidden sm:table-cell">Email</TableHead>
+                  <TableHead>Total</TableHead>
+                  <TableHead>Status</TableHead>
+                  <TableHead className="hidden sm:table-cell">
+                    Invoice Number
+                  </TableHead>
+                  <TableHead className="text-center">Action</TableHead>
+                </TableRow>
+              </TableHeader>
+              <OrdersComponent orders={orders} />
+            </Table>
+            <ScrollBar orientation="horizontal" />
+          </ScrollArea>
+        </CardContent>
+      </Card>
+    </Container>
+  );
 };
 
 export default OrdersPage;
